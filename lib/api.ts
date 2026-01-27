e25fcd8363de6e608eea2fbc9d3d69b85fe53c19
ni3rav <mahetan35@gmail.com>
2026-01-28 00:39:09 +0530

    elysia setup

---
+++ lib/api.ts
@@ -0,0 +1,10 @@
+import { treaty } from "@elysiajs/eden";
+import { app } from "@/app/api/[[...slugs]]/route";
+import { env } from "@/lib/env";
+
+// .api to enter /api prefix
+export const api =
+  // process is defined on server side and build time
+  typeof process !== "undefined"
+    ? treaty(app).api
+    : treaty<typeof app>(env.NEXT_PUBLIC_APP_BASE_URL).api;
