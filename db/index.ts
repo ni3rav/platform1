f8096c57fdbf2897aa646f4e20b0a7bbfe138fce
ni3rav <mahetan35@gmail.com>
2026-02-04 20:48:12 +0530

    chore: remove drizzle and switching to bun for sql

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,2 @@
-import { drizzle } from "drizzle-orm/neon-http";
-// import * as schema from "@/db/schema";
-import { env } from "@/lib/env";
-
-export const db = drizzle(env.DATABASE_URL);
+import { sql } from "bun";
+export const db = sql;
--- db/schema/index.ts
+++ db/schema/index.ts
@@ -1,1 +1,1 @@
-export const schema = {};
+export {}
