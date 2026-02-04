5964ed600869ca37b3340c3e084b0d68a03d245e
ni3rav <mahetan35@gmail.com>
2026-02-04 21:37:53 +0530

    fix: regex fix

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 const ACCEPTED_BRANCHES = ["cse", "ict", "cie"] as const;
 
 export const VALID_EMAIL_REGEX = new RegExp(
-  `^[a-zA-Z]+\\.(${ACCEPTED_BRANCHES.join("|")})\\d{2}@adaniuni\\.ac\\.in$`,
+  `^[a-zA-Z]+\.(${ACCEPTED_BRANCHES.join("|")})([2][1-9]|[3][0])@adaniuni\.ac\.in$`,
 );
 export const REDIS_OTP_TTL = 300;
 export const RATE_LIMIT_TTL = 300; // 5 minutes
