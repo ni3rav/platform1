2e6fd7a7197f7c716cfe5b8e2f19bb19715d4d5a
ni3rav <mahetan35@gmail.com>
2026-04-22 10:48:32 +0530

    fix: switch to neon driver

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,6 @@
 import { env } from "@/lib/env";
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/neon-http";
 import * as postsSchema from "./schema/posts";
 import * as commentsSchema from "./schema/comments";
 import * as reportsSchema from "./schema/reports";
